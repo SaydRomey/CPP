5bda4fdd94b065247baa16b61bb74fcad93d98ba
Cederic Dumais <cdumais@c3r1p2.42quebec.com>
2024-01-11 13:35:53 -0500

    ceepeepee

--- Info/src/main.cpp
+++ Info/src/main.cpp
@@ -6,45 +6,13 @@
 /*   By: cdumais <cdumais@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/04 10:52:46 by cdumais           #+#    #+#             */
-/*   Updated: 2024/01/11 11:27:33 by cdumais          ###   ########.fr       */
+/*   Updated: 2024/01/11 12:01:57 by cdumais          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 
-void	byPtr(std::string* str)
-{
-	*str += " there";
-}
-
-void	byConstPtr(std::string const *str)
-{
-	std::cout << *str << std::endl;
-}
-
-void	byRef(std::string& str)
-{
-	str += " there";
-}
-
-void	byConstRef(std::string const &str)
-{
-	std::cout << str << std::endl;
-}
-
 int	main(void)
 {
-	std::string	str = "Hello";
-
-	std::cout << str << std::endl;
-	byPtr(&str);
-	byConstPtr(&str);
-
-	str = "Are you";
-	
-	std::cout << str << std::endl;
-	byRef(str);
-	byConstRef(str);
-
 	return (0);
 }
