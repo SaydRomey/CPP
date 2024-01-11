7ff211e7803278e60a781ae7349a6687ca28d14c
Cederic Dumais <cdumais@c3r1p2.42quebec.com>
2024-01-11 12:20:17 -0500

    brain

---
+++ CPP01/ex02/src/main.cpp
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cdumais <cdumais@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/11 12:06:00 by cdumais           #+#    #+#             */
+/*   Updated: 2024/01/11 12:19:48 by cdumais          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+
+int	main(void)
+{
+	std::string		str = "HI THIS IS BRAIN";
+	std::string*	stringPTR = &str;
+	std::string&	stringREF = str;
+
+	std::cout << "Memory adress of the string variable:\t" << &stringPTR << std::endl;
+	std::cout << "Memory adress held by 'stringPTR':\t" << stringPTR << std::endl;
+	std::cout << "Memory adress held by 'stringREF':\t" << &stringREF << std::endl;
+
+	std::cout << "Value of the string variable:\t\t" << str << std::endl;
+	std::cout << "Value pointed to by 'stringPTR:\t\t" << *stringPTR << std::endl;
+	std::cout << "Value pointed to by 'stringREF':\t" << stringREF << std::endl;
+
+	return (0);
+}
