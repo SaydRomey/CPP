7f775c43cc8c06212b3fd106624988fe7b8ba805
Cederic Dumais <cdumais@c3r1p2.42quebec.com>
2024-01-11 11:20:56 -0500

    reference

--- Info/src/main.cpp
+++ Info/src/main.cpp
@@ -6,38 +6,26 @@
 /*   By: cdumais <cdumais@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/04 10:52:46 by cdumais           #+#    #+#             */
-/*   Updated: 2024/01/11 11:11:00 by cdumais          ###   ########.fr       */
+/*   Updated: 2024/01/11 11:17:38 by cdumais          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 
-class	Student
-{
-	private:
-		std::string	_login;
-	
-	public:
-		Student(void) : _login("ldefault")
-		{
-			std::cout << "Student " << this->_login << " is born" << std::endl;
-		}
-		
-		~Student(void)
-		{
-			std::cout << "Student " << this->_login << " died" << std::endl;
-		}
-		
-};
-
 int	main(void)
 {
-	Student	*students = new Student[42];
+	int		numberOfBalls = 42;
 
-	// Do some stuff here
+	int*	ballsPtr = &numberOfBalls;
+	int&	ballsRef = numberOfBalls;
 
-	delete [] students;
-	
-	
-	return (0); //bob is destroyed
+	std::cout << numberOfBalls << " " << *ballsPtr << " " << ballsRef << std::endl;
+
+	*ballsPtr = 21;
+	std::cout << numberOfBalls << std::endl;
+
+	ballsRef = 84;
+	std::cout << numberOfBalls << std::endl;
+
+	return (0);
 }
