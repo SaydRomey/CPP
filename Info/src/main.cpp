5dcd80de4091a5da755e8571300efd61fef98310
Cederic Dumais <cdumais@c3r1p2.42quebec.com>
2024-01-11 11:54:01 -0500

    modify by ptr or ref

--- Info/src/main.cpp
+++ Info/src/main.cpp
@@ -6,26 +6,45 @@
 /*   By: cdumais <cdumais@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/04 10:52:46 by cdumais           #+#    #+#             */
-/*   Updated: 2024/01/11 11:17:38 by cdumais          ###   ########.fr       */
+/*   Updated: 2024/01/11 11:27:33 by cdumais          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 
+void	byPtr(std::string* str)
+{
+	*str += " there";
+}
+
+void	byConstPtr(std::string const *str)
+{
+	std::cout << *str << std::endl;
+}
+
+void	byRef(std::string& str)
+{
+	str += " there";
+}
+
+void	byConstRef(std::string const &str)
+{
+	std::cout << str << std::endl;
+}
+
 int	main(void)
 {
-	int		numberOfBalls = 42;
+	std::string	str = "Hello";
 
-	int*	ballsPtr = &numberOfBalls;
-	int&	ballsRef = numberOfBalls;
+	std::cout << str << std::endl;
+	byPtr(&str);
+	byConstPtr(&str);
 
-	std::cout << numberOfBalls << " " << *ballsPtr << " " << ballsRef << std::endl;
-
-	*ballsPtr = 21;
-	std::cout << numberOfBalls << std::endl;
-
-	ballsRef = 84;
-	std::cout << numberOfBalls << std::endl;
+	str = "Are you";
+	
+	std::cout << str << std::endl;
+	byRef(str);
+	byConstRef(str);
 
 	return (0);
 }
