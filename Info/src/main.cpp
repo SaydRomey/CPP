ac50cfd1ff1e1d3a03e3edb88fd290dead8a6e5d
Sayd Romey <sayd@null.net>
2024-01-15 11:44:20 -0500

    input and output file stream testing

--- Info/src/main.cpp
+++ Info/src/main.cpp
@@ -6,13 +6,28 @@
 /*   By: cdumais <cdumais@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/04 10:52:46 by cdumais           #+#    #+#             */
-/*   Updated: 2024/01/11 12:01:57 by cdumais          ###   ########.fr       */
+/*   Updated: 2024/01/15 11:37:15 by cdumais          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
+#include <fstream>
 
 int	main(void)
 {
+	std::ifstream	ifs("numbers");
+	unsigned int	dst;
+	unsigned int	dst2;
+	ifs >> dst >> dst2;
+
+	std::cout << dst << " " << dst2 << std::endl;
+	ifs.close();
+	// 
+	
+	std::ofstream	ofs("test.out");
+
+	ofs << "This is a testing string" << std::endl;
+	ofs.close();
+	
 	return (0);
 }
