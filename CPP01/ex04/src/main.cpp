90218a3086e8f356d7dc9500cd3564dc57b5db40
Sayd Romey <sayd@null.net>
2024-01-15 13:50:17 -0500

    edit stream

--- CPP01/ex04/src/main.cpp
+++ CPP01/ex04/src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: cdumais <cdumais@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/11 21:59:38 by cdumais           #+#    #+#             */
-/*   Updated: 2024/01/12 21:13:02 by cdumais          ###   ########.fr       */
+/*   Updated: 2024/01/15 13:47:38 by cdumais          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,71 +14,87 @@
 #include <fstream>
 #include <string>
 
-std::string replaceInString(const std::string& str, const std::string& from, const std::string& to)
+#define SUFFIX	".replace"
+
+void	openInputFile(std::ifstream& inFile, const std::string& filename)
 {
-	std::string result;
-	size_t pos = 0;
-	size_t from_len = from.length();
+	inFile.open(filename.c_str());
+	if (!inFile.is_open())
+		std::cerr << "Error opening file: " << filename << std::endl;
+}
+
+void	openOutputFile(std::ofstream& outStream, const std::string& filename)
+{
+	std::string	outFile = filename + SUFFIX;
+	outStream.open(outFile.c_str());
+	if (!outStream.is_open())
+		std::cerr << "Error creating file: " << outFile << std::endl;
+}
+
+std::string	replaceInString(const std::string& str, const std::string& from, const std::string& to)
+{
+	std::string	result;
+	size_t		index = 0;
+	size_t		from_len = from.length();
 
 	while (true)
 	{
-		size_t found = str.find(from, pos);
+		size_t	found = str.find(from, index);
 
 		if (found != std::string::npos)
 		{
-			result.append(str, pos, found - pos);
+			result.append(str, index, found - index);
 			result.append(to);
-			pos = found + from_len;
+			index = found + from_len;
 		}
 		else
 		{
-			result.append(str, pos, str.length() - pos);
+			result.append(str, index, str.length() - index);
 			break;
 		}
 	}
-	return result;
+	return (result);	
 }
 
-void processFile(const char* filename, const std::string& s1, const std::string& s2)
+void	processLines(std::ifstream& inFile, std::ofstream& outStream, const std::string& s1, const std::string& s2)
 {
-	std::ifstream inFile(filename);
-	if (!inFile.is_open())
-	{
-		std::cerr << "Error opening file: " << filename << std::endl;
-		return;
-	}
-
-	std::string outFile = std::string(filename) + ".replace";
-	std::ofstream outStream(outFile.c_str());
-	if (!outStream.is_open())
-	{
-		std::cerr << "Error creating file: " << outFile << std::endl;
-		return;
-	}
-
-	std::string line;
+	std::string	line;
+	bool		isFirstLine = true;
+	
 	while (getline(inFile, line))
 	{
-		outStream << replaceInString(line, s1, s2);
-		if (!inFile.eof())
-		{
+		if (!isFirstLine)
 			outStream << std::endl;
-		}
+		outStream << replaceInString(line, s1, s2);
+		isFirstLine = false;
 	}
-
-	inFile.close();
-	outStream.close();
 }
-
 
 int main(int argc, char *argv[])
 {
 	if (argc != 4)
 	{
 		std::cerr << "Usage: " << argv[0] << " <filename> <string1> <string2>" << std::endl;
-		return 1;
+		return (1);
 	}
+	std::string	filename = argv[1];
+	std::string	s1 = argv[2];
+	std::string	s2 = argv[3];
 
-	processFile(argv[1], argv[2], argv[3]);
-	return 0;
+	std::ifstream	inFile;
+	openInputFile(inFile, filename);
+	if (!inFile.is_open())
+		return (1);
+
+	std::ofstream	outStream;
+	openOutputFile(outStream, filename);
+	if (!outStream.is_open())
+		return (1);
+
+	processLines(inFile, outStream, s1, s2);
+
+	inFile.close();
+	outStream.close();	
+
+	return (0);
 }
