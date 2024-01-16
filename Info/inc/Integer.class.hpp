d9a890bf30aced69ce4ddbb7691ab4b3f0e28401
Cederic Dumais <cdumais@c2r3p4.42quebec.com>
2024-01-16 15:26:45 -0500

    Operator overload

--- Info/inc/Integer.class.hpp
+++ Info/inc/Integer.class.hpp
@@ -1,33 +1,35 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.cpp                                           :+:      :+:    :+:   */
+/*   Integer.class.hpp                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: cdumais <cdumais@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/01/04 10:52:46 by cdumais           #+#    #+#             */
-/*   Updated: 2024/01/16 15:15:21 by cdumais          ###   ########.fr       */
+/*   Created: 2024/01/16 14:34:29 by cdumais           #+#    #+#             */
+/*   Updated: 2024/01/16 14:39:33 by cdumais          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "Integer.class.hpp"
+#ifndef INTEGER_CLASS_HPP
+# define INTEGER_CLASS_HPP
+
 #include <iostream>
-#include <fstream>
 
-int	main(void)
+class	Integer
 {
-	Integer		x(30);
-	Integer		y(10);
-	Integer		z(0);
+	public:
+		Integer(int const n);
+		~Integer(void);
 
-	std::cout << "Value of x : " << x << std::endl;
-	std::cout << "Value of y : " << y << std::endl;
-	y = Integer(12);
-	std::cout << "Value of y : " << y << std::endl;
+		int	getValue(void) const;
 
-	std::cout << "Value of z : " << z << std::endl;
-	z = x + y;
-	std::cout << "Value of z : " << z << std::endl;
+		Integer &	operator=(Integer const & rhs);
+		Integer		operator+(Integer const & rhs) const;
+	
+	private:
+		int	_n;
+};
 
-	return (0);
-}
+std::ostream & operator<<(std::ostream & o, Integer const & rhs);
+
+#endif
