085e30057b832f0670c102cd98a2adce674d54b1
Cederic Dumais <cdumais@c3r1p3.42quebec.com>
2024-01-24 16:56:59 -0500

    Polymorphs assemble

--- CPP04/ex00/src/main.cpp
+++ CPP04/ex00/src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: cdumais <cdumais@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/23 15:56:02 by cdumais           #+#    #+#             */
-/*   Updated: 2024/01/23 19:32:37 by cdumais          ###   ########.fr       */
+/*   Updated: 2024/01/24 16:55:21 by cdumais          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,16 +16,28 @@
 #include "WrongAnimal.hpp"
 #include "WrongCat.hpp"
 
+using std::string;
+using std::cout;
+using std::endl;
+
 #define RESET	"\033[0m"
+#define BOLD	"\033[1m"
+#define PURPLE	"\033[95m"
 #define ORANGE	"\033[38;5;208m"
+
+#define PRINT(STR)	cout << BOLD << "\n[ " << STR << " ]" << RESET << endl;
 
 void	print(std::string str, std::string color = ORANGE)
 {
-	std::cout << color << "\n[ " << str << " ]" << RESET << std::endl;
+	cout << color << "\n[ " << str << " ]" << RESET << endl;
 }
 
-void	goodTest(void)
+/* ************************************************************************** */
+
+void	pdfTest(void)
 {
+	print("*** PDF TEST ***", PURPLE);
+
 	print("Animal* 'beast' = new Animal()");
 	const Animal*	beast = new Animal();
 	print("Animal* 'puppy' = new Dog()");
@@ -34,9 +46,9 @@ void	goodTest(void)
 	const Animal*	kitten = new Cat();
 
 	print("puppy->getType()");
-	std::cout << puppy->getType() << std::endl;
+	cout << puppy->getType() << endl;
 	print("kitten->getType()");
-	std::cout << kitten->getType() << std::endl;
+	cout << kitten->getType() << endl;
 
 	print("kitten->makeSound()");
 	kitten->makeSound();
@@ -55,6 +67,8 @@ void	goodTest(void)
 
 void	wrongTest(void)
 {
+	print("*** WRONG TEST ***", PURPLE);
+
 	print("WrongAnimal* 'beast' = new WrongAnimal()");
 	const WrongAnimal*	beast = new WrongAnimal();
 	print("Animal* 'puppy' = new Dog()");
@@ -63,9 +77,9 @@ void	wrongTest(void)
 	const WrongAnimal*	kitten = new WrongCat();
 
 	print("puppy->getType()");
-	std::cout << puppy->getType() << std::endl;
+	cout << puppy->getType() << endl;
 	print("kitten->getType()");
-	std::cout << kitten->getType() << std::endl;
+	cout << kitten->getType() << endl;
 
 	print("kitten->makeSound()");
 	kitten->makeSound();
@@ -82,9 +96,34 @@ void	wrongTest(void)
 	delete beast;
 }
 
+void	copyConstructorTest(void)
+{
+	print("*** COPY TEST ***", PURPLE);
+
+	print("Dog* puppy = new Dog()");
+	Dog*	puppy = new Dog();
+	print("Dog  fluffyboy(*puppy)");
+	Dog		fluffyboy(*puppy);
+
+	print("puppy->getType() + puppy->makeSound()");
+	cout << puppy->getType() << ": ";
+	puppy->makeSound();
+	print("delete puppy");
+	delete puppy;
+
+	print("fluffyboy.getType() + fluffyboy.makeSound()");
+	cout << fluffyboy.getType() << ": ";
+	fluffyboy.makeSound();
+}
+
 int	main(void)
 {
-	goodTest();
-	// wrongTest();
+	pdfTest();
+
+	wrongTest();
+
+	copyConstructorTest();
+
+	PRINT((string)"this is " + (string)"a test");
 	return (0);
 }
