71aecb1260edcc613693a65d935706aee7f03843
Cederic Dumais <cdumais@c3r5p1.42quebec.com>
2024-07-12 20:03:03 -0400

    casting

--- CPP06/ex00/src/main.cpp
+++ CPP06/ex00/src/main.cpp
@@ -1,7 +1,31 @@
-#include <iostream>
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cdumais <cdumais@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/12 17:25:14 by cdumais           #+#    #+#             */
+/*   Updated: 2024/07/12 19:59:25 by cdumais          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
-int	main(void)
+#include "Converter.hpp"
+
+using std::cout;
+using std::endl;
+using std::string;
+
+int	main(int argc, char *argv[])
 {
-	std::cout << "Don't panic !" << std::endl;
+	if (argc != 2)
+	{
+		cout << "Usage: " << argv[0] << " <literal>" << endl;
+		return (1);
+	}
+
+	Converter scalar(argv[1]);
+	scalar.convert();
+
 	return (0);
 }
